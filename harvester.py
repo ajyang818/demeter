6c4542c43c35e4613cd587cce1f7a3a3ef4cd51e
Allen Yang <allen@yipit.com>
2014-01-08 21:45:39 -0500

    (Closes #3) (Closes #13) Outputing to csv; cleaning up text

--- harvester.py
+++ harvester.py
@@ -1,10 +1,9 @@
 from bs4 import BeautifulSoup
-from lxml import etree
+import csv
 import re
 import requests
-from StringIO import StringIO
-
-parser = etree.XMLParser(remove_blank_text=True, strip_cdata=False, ns_clean=True, encoding='utf-8', recover=True)
+import string
+import unicodedata
 
 
 def get_user_reviews(url):
@@ -20,8 +19,17 @@ def get_user_reviews(url):
 
 class YelpProfileHarvester(object):
 
-    def __init__(self, url):
+    DEFAULT_EXCEL_OUTPUT_FILE = 'test_output.csv'
+
+    def __init__(self, url, excel_output=False, clear=False):
         self.url = url
+        self.excel_output = excel_output
+        if clear:
+            clearing_file = open(self.DEFAULT_EXCEL_OUTPUT_FILE, "w+")
+            clearing_file.close()
+        if excel_output:
+            self.file = open(self.DEFAULT_EXCEL_OUTPUT_FILE, 'a')
+            self.writer = csv.writer(self.file)
 
     def initiate(self):
 
@@ -36,10 +44,19 @@ class YelpProfileHarvester(object):
                     raise YelpProfilePageError('No reviews found on first page for this user')
                 break
             for review in reviews:
-                parsed_reviews.append(self.parse_individual_review(review))
+                parsed_review = self.parse_individual_review(review)
+                if self.excel_output == True:
+                    self._output_to_excel(parsed_review)
+                parsed_reviews.append(parsed_review)
             review_counter += 10
 
+        if self.excel_output:
+            self.file.close()
+
         return parsed_reviews
+
+    ######################################################
+    # "Getting" methods
 
     def _get_reviews_from_url(self, url):
         response = requests.get(url)
@@ -55,7 +72,24 @@ class YelpProfileHarvester(object):
         return soup.find_all('div', 'review clearfix')
 
     ######################################################
+    # "Outputting" methods
+
+    def _output_to_excel(self, parsed_review):
+        self.writer.writerow([
+            parsed_review['review_date'],
+            parsed_review['business_name'],
+            parsed_review['review_id'],
+            parsed_review['stars'],
+            parsed_review['address'],
+            parsed_review['neighborhood'],
+            parsed_review['categories'],
+            parsed_review['business_link'],
+            parsed_review['review'],
+        ])
+
+    ######################################################
     # Individual Review Parser Methods
+
     def parse_individual_review(self, review):
         """
         Goal: dictionary per deal = {
@@ -74,7 +108,8 @@ class YelpProfileHarvester(object):
         indiv_review_data = {}
 
         business_name_block = review.h4
-        indiv_review_data['business_name'] = self._get_first_stripped_string(business_name_block)
+        raw_business_name = self._get_first_stripped_string(business_name_block)
+        indiv_review_data['business_name'] = clean_weird_characters(raw_business_name)
 
         address_block = review.find('address')
         indiv_review_data['address'] = self._get_concatenated_string_from_stripped_strings(address_block)
@@ -90,7 +125,8 @@ class YelpProfileHarvester(object):
         indiv_review_data['stars'] = self._get_block_attribute(stars_block, 'alt', 0, 3)
 
         review_block = review.find('div', 'review_comment')
-        indiv_review_data['review'] = self._get_first_stripped_string(review_block)
+        review_text = self._get_first_stripped_string(review_block)
+        indiv_review_data['review'] = clean_weird_characters(review_text)
 
         review_id_block = review.find('div', 'rateReview')
         indiv_review_data['review_id'] = self._get_block_attribute(review_id_block, 'data-review-id', 0)
@@ -104,11 +140,11 @@ class YelpProfileHarvester(object):
         return html_block.get_text(strip=True)
 
     def _get_concatenated_string_from_stripped_strings(self, html_block, delimiter='|'):
-        strings = [string for string in html_block.stripped_strings]
+        strings = [clean_string for clean_string in html_block.stripped_strings]
         return delimiter.join(strings)
 
     def _get_category_list(self, html_block):
-        return [string.get_text(strip=True) for string in html_block.find_all('a')]
+        return [dirty_string.get_text(strip=True) for dirty_string in html_block.find_all('a')]
 
     def _get_neighborhod(self, html_block):
         neighborhood_string = html_block.find(text=re.compile("Neighborhood"))
@@ -127,6 +163,10 @@ class YelpProfileHarvester(object):
             return star_string[start_char:stop_char]
 
 
+# Shamelessly taken from StackOverflow
+def clean_weird_characters(messy_string):
+   intermediate_string = ''.join((ch for ch in unicodedata.normalize('NFD', unicode(messy_string)) if unicodedata.category(ch) != 'Mn'))
+   return filter(lambda ch: ch in string.printable, intermediate_string)
 
 
 class YelpProfilePageError(Exception):
