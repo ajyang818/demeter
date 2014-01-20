9ab332fb7b57eee6bd371fc6495ac30f404e2f14
Allen Yang <allen@yipit.com>
2014-01-20 12:56:20 -0500

    Admin pages

--- business/admin.py
+++ business/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from business.models import Business, Category, Neighborhood
 
-# Register your models here.
+
+class BusinessAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class NeighborhoodAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Business, BusinessAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Neighborhood, NeighborhoodAdmin)
--- yelper/admin.py
+++ yelper/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from yelper.models import Yelper
 
-# Register your models here.
+
+class YelperAdmin(admin.ModelAdmin):
+    list_display = ('display_name', 'location', 'reviews', 'last_harvest')
+
+
+admin.site.register(Yelper, YelperAdmin)
