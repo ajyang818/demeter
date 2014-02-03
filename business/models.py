279f2b6d1488250f270eef271a2ac4b921e7bde0
Allen Yang <allen@yipit.com>
2014-02-02 22:18:12 -0500

    Building out Category model, with mptt

--- business/models.py
+++ business/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Neighborhood(models.Model):
@@ -8,13 +9,17 @@ class Neighborhood(models.Model):
     state = models.CharField(max_length=2)
 
 
-class Category(models.Model):
-    # TODO: Nesting / Tree relationship
-    name = models.CharField(max_length=100)
+class Category(MPTTModel):
+    slug = models.SlugField(primary_key=True, max_length=200)
+    name = models.CharField(max_length=200)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
 
 
 class Business(models.Model):
-    business_yelp_slug = models.CharField(primary_key=True, max_length=100)
+    business_yelp_slug = models.SlugField(primary_key=True, max_length=100)
     name = models.CharField(max_length=100)
     category = models.ManyToManyField(Category)
     is_closed = models.BooleanField(default=False)
@@ -24,7 +29,7 @@ class Business(models.Model):
     phone = models.IntegerField(blank=True, null=True)
 
     review_count = models.IntegerField(blank=True, null=True)  # Do I need this?
-    display_stars = models.DecimalField(decimal_places=1, max_digits=2, blank=True, null=True)
+    stars = models.DecimalField(decimal_places=1, max_digits=2, blank=True, null=True)
 
     elite_count = models.IntegerField(blank=True, null=True)
     elite_stars = models.DecimalField(decimal_places=4, max_digits=5, blank=True, null=True)
